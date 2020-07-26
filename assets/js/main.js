2de4a1eae89a413824378564d99b7b712f963acd
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-07-26 22:11:11 +0100

    add slickjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,32 @@ const header = document.querySelector('.nav-mobile')
 hamburger.addEventListener('click', () => {
       header.classList.toggle('toggle');
 })
+
+
+
+$('.responsive').slick({
+      autoplay: true,
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
